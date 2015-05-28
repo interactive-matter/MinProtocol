a5d5bd9e273e9b3169f5bd693684212a5122740e
Interactive Matter <info@interactive-matter.org>
2015-05-28 16:52:49 +0200

    increasing the buffer

--- min.h
+++ min.h
@@ -16,7 +16,7 @@
  *
  * 3 (header) + 1 (id) + 1 (control) + 15 (payload) + 2 (checksum) + 1 (EOF) + stuff bytes
  */
-#define MAX_FRAME_PAYLOAD_SIZE		(128U)
+#define MAX_FRAME_PAYLOAD_SIZE		(256U)
 
 /* Initialize Layer 1 */
 void min_init_layer1();
